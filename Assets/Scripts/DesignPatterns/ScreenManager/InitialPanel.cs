f000b3651b597c054f59d2983c8b65a158b1dd30
Nanox42 <100004866+Nanox42@users.noreply.github.com>
2023-11-30 01:14:20 -0300

    cambio con el screen manager

--- Assets/Scripts/DesignPatterns/ScreenManager/InitialPanel.cs
+++ Assets/Scripts/DesignPatterns/ScreenManager/InitialPanel.cs
@@ -7,8 +7,16 @@ public class InitialPanel : Panel
     [SerializeField]
     private Panel instructionPanel;
 
+    [SerializeField]
+    private Panel ChoosePlayer;
+
     public void OpenInstructionPanel()
     {
         ScreenManager.Instance.Push(instructionPanel);
+    }
+
+    public void OpenChoosePlayer()
+    {
+        ScreenManager.Instance.Push(ChoosePlayer);
     }
 }
